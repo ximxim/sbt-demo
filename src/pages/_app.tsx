0d052eda03a4908642cfd4a04b4ef85ee44ff13d
Azim Ahmed <azim.ahmed7@gmail.com>
2023-03-13 20:30:17 -0400

    service ready

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
-import 'kiki/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "sbt/styles/globals.css";
+import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
